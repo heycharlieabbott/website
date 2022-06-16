ebdc98c6d5a11e4465c16ef892b0e7dbcc5c66ea
heycharlieabbott <chzabbott@gmail.com>
2022-06-16 22:21:02 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/main.scss";
+import "./styles/globals/_boilerplate.scss";
+
+console.log("heyoasldjfh");
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.[contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Charlie Abbott",
+      filename: "index.html",
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+};
