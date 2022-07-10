dda4e4a315607d4f7267846ac0b9fabcdbea9915
heycharlieabbott <chzabbott@gmail.com>
2022-07-10 13:45:08 +0900

    add mobile responsiveness, remove animations scss

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: ["./src/canvas.js", "./src/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
